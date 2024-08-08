8dcb9b511997c9c342befd78f52c09a75e93a7f7
ManuLosta <mlostalom@gmail.com>
2024-08-07 22:17:41 -0300

    feat: parser parses call to print function

--- parser/src/main/kotlin/Parser.kt
+++ parser/src/main/kotlin/Parser.kt
@@ -11,11 +11,12 @@ class Parser(private val tokens: List<Token>) {
         while (!isAtEnd()) {
             when (peek().type) {
                 TokenType.LET_KEYWORD -> program.add(parseDeclaration())
+                TokenType.PRINT -> program.add(parseCall())
                 else -> program.add(parseExpression())
             }
         }
 
-        return emptyList()
+        return program
     }
 
     private fun advance(): Token {
@@ -36,31 +37,39 @@ class Parser(private val tokens: List<Token>) {
     }
 
     private fun parseDeclaration(): DeclarationNode {
-        advance() // ignore let keyword
+        val start = advance().start // ignore let keyword
         if (peek().type != TokenType.IDENTIFIER) throw Exception("Error parsing: Expected an identifier")
 
         val identifierToken = advance()
 
-        if (peek().type != TokenType.COLON) throw Exception("Error parsing: Expected a ':'")
+        if (peek().type != TokenType.COLON)
+            throw Exception("Error parsing: Expected ':' after identifier")
+
         advance() // ignore colon
 
         val type = parseTypeDeclaration()
 
-        if (peek().type != TokenType.ASSIGNATION) throw Exception("Error parsing: Expected an '='")
-        advance() // ignore assignation
+        if (peek().type != TokenType.ASSIGNATION)
+            throw Exception("Error parsing: Expected '='")
 
+        advance() // ignore assignation
         val value = parseExpression()
+        val end = advance().end // Ignore semicolon
 
         return DeclarationNode(
             IdentifierNode(type, identifierToken.literal, identifierToken.start, identifierToken.end),
             value,
-            identifierToken.start, // Check
-            identifierToken.end // Check
+            start,
+            end,
         )
     }
 
     private fun parseTypeDeclaration(): Type {
-        if (peek().type != TokenType.NUMBER_TYPE || peek().type != TokenType.STRING_TYPE) throw Exception("Error parsing: Expected type definition")
+        if (
+            peek().type != TokenType.NUMBER_TYPE &&
+            peek().type != TokenType.STRING_TYPE
+            )
+            throw Exception("Error parsing: Expected type definition")
         return when (advance().type) {
             TokenType.NUMBER_TYPE -> Type.NUMBER
             TokenType.STRING_TYPE -> Type.STRING
@@ -68,8 +77,19 @@ class Parser(private val tokens: List<Token>) {
         }
     }
 
-    // TODO: Parse Expression
+    // TODO: Parse Expression (SUM MUL DIV, ETC)
     private fun parseExpression(): ASTNode {
-        return LiteralNode("6", Type.NUMBER)
+        return when (peek().type) {
+            TokenType.NUMBER_LITERAL -> LiteralNode(advance().literal, Type.NUMBER)
+            TokenType.STRING_LITERAL -> LiteralNode(advance().literal, Type.STRING)
+            else -> throw Exception("Error parsing expression")
+        }
+    }
+
+    private fun parseCall(): ASTNode {
+        advance()
+        return PrintNode(
+           parseExpression()
+        )
     }
 }
--- parser/src/test/kotlin/ParserTest.kt
+++ parser/src/test/kotlin/ParserTest.kt
@@ -1,9 +1,6 @@
 import org.example.Token
 import org.example.TokenType
-import org.example.ast.DeclarationNode
-import org.example.ast.IdentifierNode
-import org.example.ast.LiteralNode
-import org.example.ast.Type
+import org.example.ast.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -11,13 +8,14 @@ class ParserTest {
     @Test
     fun `should return ast`() {
         val tokens: List<Token> = listOf(
-            Token(TokenType.LET_KEYWORD, "let", 0, 3),
+            Token(TokenType.LET_KEYWORD, "let", 0, 2),
+            Token(TokenType.IDENTIFIER, "a", 4, 4),
             Token(TokenType.COLON, ":", 4, 5),
             Token(TokenType.NUMBER_TYPE, "number", 6, 12),
-            Token(TokenType.IDENTIFIER, "a", 13, 14),
             Token(TokenType.ASSIGNATION, "=", 16, 17),
             Token(TokenType.NUMBER_LITERAL, "6", 18, 19),
             Token(TokenType.SEMICOLON, ";", 20, 21),
+            Token(TokenType.EOF, "eof", 22, 23)
         )
 
         val parser = Parser(tokens)
@@ -25,13 +23,35 @@ class ParserTest {
 
         val expected = listOf(
             DeclarationNode(
-                IdentifierNode(Type.NUMBER, "a", 13, 14),
+                IdentifierNode(Type.NUMBER, "a", 4, 4),
                 LiteralNode("6", Type.NUMBER),
                 0, 21
-
             )
         )
 
         assertEquals(ast, expected)
+    }
+
+    @Test
+    fun `test print function`() {
+        val tokens: List<Token> = listOf(
+            Token(TokenType.PRINT, "print", 0, 5),
+            Token(TokenType.STRING_LITERAL, "hola", 3, 6),
+            Token(TokenType.EOF, "eof", 8, 8)
+        )
+
+        val parser = Parser(tokens)
+        val ast = parser.parse()
+
+        val expected = listOf(
+            PrintNode(
+                LiteralNode(
+                    "hola",
+                    Type.STRING
+                )
+            )
+        )
+
+        assertEquals(expected, ast)
     }
 }
