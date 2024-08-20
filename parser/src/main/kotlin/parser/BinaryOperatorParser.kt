6379b255e0d4632063c8b342f4e91efb1ad889fe
ManuLosta <mlostalom@gmail.com>
2024-08-19 23:04:30 -0300

    format code

--- parser/src/main/kotlin/parser/BinaryOperatorParser.kt
+++ parser/src/main/kotlin/parser/BinaryOperatorParser.kt
@@ -5,8 +5,12 @@ import ast.OperatorExpr
 import org.example.Token
 import org.example.TokenType
 
-class BinaryOperatorParser(private val precedence: Int): InfixParser {
-    override fun parse(parser: Parser, left: Expression, token: Token): Expression {
+class BinaryOperatorParser(private val precedence: Int) : InfixParser {
+    override fun parse(
+        parser: Parser,
+        left: Expression,
+        token: Token,
+    ): Expression {
         val right = parser.parsePrecedence(precedence)
         parser.matchAny(
             TokenType.SUM,
@@ -14,7 +18,7 @@ class BinaryOperatorParser(private val precedence: Int): InfixParser {
             TokenType.MUL,
             TokenType.DIV,
         )
-        return OperatorExpr(left,  token.literal, right, token.start)
+        return OperatorExpr(left, token.literal, right, token.start)
     }
 
     override fun getPrecedence(): Int {
