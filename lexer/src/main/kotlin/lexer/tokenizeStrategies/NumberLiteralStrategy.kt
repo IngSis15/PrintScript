8b629c727e962ac3cd110dc29f013fb038fba2a0
LautiGR_116 <lreinoso@mail.austral.edu.ar>
2024-09-10 14:32:04 -0300

    Fix Lexer float parsing and tests implemented

--- lexer/src/main/kotlin/lexer/tokenizeStrategies/NumberLiteralStrategy.kt
+++ lexer/src/main/kotlin/lexer/tokenizeStrategies/NumberLiteralStrategy.kt
@@ -10,10 +10,21 @@ class NumberLiteralStrategy : TokenizeStrategy {
     override fun lex(lexer: Lexer): Token {
         val word = StringBuilder()
         val startColumn = lexer.posColumn()
+        var hasDecimalPoint = false
 
         while (lexer.current() in '0'..'9' || lexer.current() == '.') {
+            if (lexer.current() == '.') {
+                if (hasDecimalPoint) {
+                    throw IllegalArgumentException("Malformed number: multiple decimal points")
+                }
+                hasDecimalPoint = true
+            }
             word.append(lexer.current())
             lexer.advance()
+        }
+
+        if (hasDecimalPoint && word.last() == '.') {
+            throw IllegalArgumentException("Malformed number: decimal point without digits")
         }
 
         return Token(TokenType.NUMBER_LITERAL, word.toString(), Position(lexer.posLine(), startColumn))
